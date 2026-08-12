19f2c96b3adc0e9aaf123775611631df760ec880
Tobias Agustin Flores <tobiasflores991019@gmail.com>
2026-08-12 19:36:14 -0300

    Agregar dashboard al build de Vite

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,21 @@ export default defineConfig({
         rolldownOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                login: resolve(__dirname, "src/pages/login/login.html"),
-                registro: resolve(__dirname, "src/pages/registro/registro.html")
+
+                login: resolve(
+                    __dirname,
+                    "src/pages/login/login.html"
+                ),
+
+                registro: resolve(
+                    __dirname,
+                    "src/pages/registro/registro.html"
+                ),
+
+                dashboard: resolve(
+                    __dirname,
+                    "src/pages/cliente/dashboard/dashboard.html"
+                )
             }
         }
     }
